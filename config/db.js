6449e9db7e065f4a1d9e620b4441a19b92b0e0a5
parvans <parvansajeevan@gmail.com>
2023-06-29 12:46:57 +0530

    mongodb connection

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host} 🫙`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 const app = express();
 dotenv.config();
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }
 );
+connectDB();
 const PORT = process.env.PORT || 6060;
 app.listen(PORT, () => console.log(`Server Started on port ${PORT} 🚀`));
